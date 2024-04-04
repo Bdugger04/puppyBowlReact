5b3e87cfcd472b32451669f3fa4e86fcc650feec
Brittany Dugger <brittanydugger@Brittanys-MacBook-Pro.local>
2024-04-04 11:23:49 -0500

    completed get call to render puppies on the page. still need to get a unique key prop

--- src/components/AllPuppies.jsx
+++ src/components/AllPuppies.jsx
@@ -1,7 +1,41 @@
-
+import {useState, useEffect} from 'react'
+import axios from'axios'
 
 export default function AllPuppies(){
+  const baseUrl = 'https://fsa-puppy-bowl.herokuapp.com/api/bdugger04/players'
+  const [players, setPlayers] = useState([])
+
+  useEffect(() => {
+    
+    async function getAllPlayers(){
+      try{
+        const {data} = await axios.get(baseUrl);
+        const players = data.data.players;
+        console.log('players', players);
+        setPlayers(players);
+      }catch (error){
+        console.error('rendering puppies didnt work bro', error)
+      }
+    }
+    getAllPlayers()
+  }, [])
   
-
-
+return(
+  <>
+  <h1>Enrolled Puppies</h1>
+  {players.map((player) => {
+    return(
+    <>
+      
+      <div key={player.name} className='puppy-card'>
+        <img src={player.imageUrl}/>
+        <h1>{player.name}</h1>
+        <h2>{player.breed}</h2>
+      </div>
+    </>
+    )
+  })}
+  
+  </>
+)
 }
